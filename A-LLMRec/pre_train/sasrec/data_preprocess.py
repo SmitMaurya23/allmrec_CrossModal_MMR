9a584bcb7ddd9b01893a86c069ac70756f562325
SmitMaurya23 <smitmaurya2004@gmail.com>
2024-11-20 00:27:09 +0530

    hi

--- A-LLMRec/pre_train/sasrec/data_preprocess.py
+++ A-LLMRec/pre_train/sasrec/data_preprocess.py
@@ -1,11 +1,30 @@
+import os
+import os.path
+import gzip
+import json
+import pickle
+from tqdm import tqdm
+from collections import defaultdict
+
+def parse(path):
+    """
+    Generator function to parse gzip-compressed JSON lines
+    """
+    g = gzip.open(path, 'rb')
+    for l in tqdm(g):
+        yield json.loads(l)
+
 def preprocess(fname):
-    countU = defaultdict(lambda: 0)
-    countP = defaultdict(lambda: 0)
+    """
+    Preprocess the dataset by counting interactions and extracting metadata.
+    """
+    countU = defaultdict(lambda: 0)  # Counting interactions for each user
+    countP = defaultdict(lambda: 0)  # Counting interactions for each item
     line = 0
 
     file_path = f'../../data/amazon/{fname}.json.gz'
 
-    # counting interactions for each user and item
+    # Counting interactions for each user and item
     for l in parse(file_path):
         line += 1
         asin = l['asin']
@@ -14,29 +33,31 @@ def preprocess(fname):
         countU[rev] += 1
         countP[asin] += 1
 
-    usermap = dict()
-    usernum = 0
-    itemmap = dict()
-    itemnum = 0
-    User = dict()
-    review_dict = {}
-    name_dict = {'title':{}, 'description':{}}
+    usermap = dict()  # Mapping from reviewerID to userID
+    usernum = 0  # User counter
+    itemmap = dict()  # Mapping from asin to itemID
+    itemnum = 0  # Item counter
+    User = dict()  # User-item interactions
+    review_dict = {}  # Dictionary of reviews and summaries for items
+    name_dict = {'title':{}, 'description':{}}  # Name dictionary for items
 
-    # Loading metadata
+    # Loading metadata (titles, descriptions, etc.)
     with open(f'../../data/amazon/meta_{fname}.json', 'r') as f:
         json_data = f.readlines()
     
-    data_list = [json.loads(line[:-1]) for line in json_data]
+    data_list = [json.loads(line[:-1]) for line in json_data]  # Load metadata into list
     meta_dict = {}
     for l in data_list:
         meta_dict[l['asin']] = l
 
+    # Processing interaction data
     for l in parse(file_path):
         line += 1
         asin = l['asin']
         rev = l['reviewerID']
         time = l['unixReviewTime']
 
+        # Set threshold for user-item interactions
         threshold = 5
         if ('Beauty' in fname) or ('Toys' in fname):
             threshold = 4
@@ -44,6 +65,7 @@ def preprocess(fname):
         if countU[rev] < threshold or countP[asin] < threshold:
             continue
 
+        # Assign user ID if not already assigned
         if rev in usermap:
             userid = usermap[rev]
         else:
@@ -52,6 +74,7 @@ def preprocess(fname):
             usermap[rev] = userid
             User[userid] = []
 
+        # Assign item ID if not already assigned
         if asin in itemmap:
             itemid = itemmap[asin]
         else:
@@ -60,6 +83,7 @@ def preprocess(fname):
             itemmap[asin] = itemid
         User[userid].append([time, itemid])
 
+        # Process reviews and summaries
         if itemmap[asin] in review_dict:
             try:
                 review_dict[itemmap[asin]]['review'][usermap[rev]] = l['reviewText']
@@ -80,10 +104,10 @@ def preprocess(fname):
             except:
                 pass
         
+        # Safely retrieve the description and title from metadata
         try:
-            # Safely retrieve the description from metadata
             item_meta = meta_dict.get(asin, {})
-            description = item_meta.get('description', '').strip()
+            description = item_meta.get('description', '').strip()  # Default to empty string if not found
 
             # Check if description is available, else set default value
             if not description:
@@ -98,7 +122,7 @@ def preprocess(fname):
         except Exception as e:
             print(f"Unexpected error with ASIN {asin}: {e}")
 
-    # Save the name dictionary to a file
+    # Save the name dictionary to a file (Pickle format)
     with open(f'../../data/amazon/{fname}_text_name_dict.pkl', 'wb') as tf:
         pickle.dump(name_dict, tf)
 
@@ -108,7 +132,7 @@ def preprocess(fname):
 
     print(usernum, itemnum)
 
-    # Write user-item interactions to file
+    # Write user-item interactions to a file
     with open(f'../../data/amazon/{fname}.txt', 'w') as f:
         for user in User.keys():
             for i in User[user]:
